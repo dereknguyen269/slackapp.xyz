7ba711c79d0d45e9a811d0339318990637f3d02d
Libra <quanmn.libra@gmail.com>
2017-04-24 13:55:57 +0700

    Fix redirect

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    sign_in @user
+    redirect_to root_path
   end
 end
