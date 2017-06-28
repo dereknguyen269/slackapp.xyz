8b829ed90bc85f50ef88172292a1009edf5649e5
Nguyen Quan <quanmn.libra@gmail.com>
2017-06-28 10:03:01 +0700

    Fix on heroku

--- config/puma.rb
+++ config/puma.rb
@@ -11,3 +11,17 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
+
+require ::File.expand_path('../../config/environment', __FILE__)
+require ::File.expand_path('../../bot/iron_man', __FILE__)
+require ::File.expand_path('../../lib/slack_ruby_bot', __FILE__)
+require ::File.expand_path('../../lib/slack_ruby_bot/commands/base', __FILE__)
+require ::File.expand_path('../../lib/slack_ruby_bot/commands/calculate', __FILE__)
+require ::File.expand_path('../../lib/slack_ruby_bot/commands/hi', __FILE__)
+require ::File.expand_path('../../lib/slack_ruby_bot/commands/hello', __FILE__)
+require ::File.expand_path('../../lib/slack_ruby_bot/support/commands_helper.rb', __FILE__)
+
+Thread.abort_on_exception = true
+Thread.new do
+  IronMan.run
+end
