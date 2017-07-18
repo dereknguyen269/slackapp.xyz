859e9ad7efaacd2f5afacb1dba7c29d42714954a
Nguyen Quan <quanmn.libra@gmail.com>
2017-07-18 16:34:24 +0700

    Simple config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,65 +1,36 @@
-server '52.90.195.71', port: 22, roles: [:web, :app, :db], primary: true
-set :repo_url, 'https://github.com/minhquan4080/slack-bot.git'
-set :application, 'slack-bot'
-set :branch, 'feature/auto-deploy'
-set :user, 'deploy'
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-set :pty, true
-set :use_sudo, false
-set :stage, :production
-set :deploy_via, :remote_cache
-set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log, "#{release_path}/log/puma.access.log"
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true # Change to false when not using ActiveRecord
+# config valid only for current version of Capistrano
+lock "3.8.2"
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-  before :start, :make_dirs
-end
+set :application, "my_app_name"
+set :repo_url, "git@example.com:me/my_repo.git"
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless "git rev-parse HEAD" == "git rev-parse origin/#{branch}"
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run git push to sync changes."
-        exit
-      end
-    end
-  end
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-  before :starting, :check_revision
-  after :finishing, :compile_assets
-  after :finishing, :cleanup
-  after :finishing, :restart
-end
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
