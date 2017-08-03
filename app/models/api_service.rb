cabb6a5cecf8a88c366321749b24a79f1012c6de
Nguyen Quan <quanmn.libra@gmail.com>
2017-08-03 15:14:26 +0700

    Add edit update delete API Services

--- app/models/api_service.rb
+++ app/models/api_service.rb
@@ -12,8 +12,25 @@
 
 class ApiService < ActiveRecord::Base
   has_many :api_service_infos, :foreign_key => :api_services_id, class_name: 'ApiServiceInfo'
-  accepts_nested_attributes_for :api_service_infos, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :api_service_infos, allow_destroy: true
 
   validates :name, uniqueness: true, presence: true
   validates :api_url, uniqueness: true, presence: true
+  validates :api_service_infos, :presence => { :message => 'there should be at least one' }
+
+  def has_info?
+    !api_service_infos.count.zero?
+  end
+
+  def avaible?
+    self.status
+  end
+
+  def single?
+    api_service_infos&.count == 1
+  end
+
+  def single
+    api_service_infos.first if single?
+  end
 end
