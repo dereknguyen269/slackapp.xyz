e08ddc57fd65278f9f4e9c705262e9d5cf073749
Nguyen Quan <quanmn.libra@gmail.com>
2017-08-13 17:55:18 +0700

    Using login with username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     sign_in_url = new_user_session_url
@@ -22,6 +23,14 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     redirect_to user_not_authorized_path, alert: "Error! You don't have permission to access."
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
 end
